40e434c5aafef97e050bc66ba1b7f951aebffe10
sergeevaa <sergeeva_ao@mail.ru>
2023-02-21 23:01:05 +0100

    §

--- server.py
+++ server.py
@@ -2,16 +2,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/flask-server', methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('path/to/git_repo')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
-    else:
-        return 'Wrong event type', 400
-
 @app.route('/')
 def home():
     user_agent = request.headers.get('User-Agent')
